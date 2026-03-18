9fc21ddb049e7fcb137821943c6d4cc51461f7da
Muhammad Ichsanudin <michsanudin04@gmail.com>
2026-03-18 13:27:02 +0700

    refactor(property,reservation): migrate endpoints to camelCase schema fields

--- api/src/features/property/property.service.ts
+++ api/src/features/property/property.service.ts
@@ -4,27 +4,14 @@ import { AppError } from "../../shared/utils/app-error.util.js";
 import type { CreatePropertyDto } from "./property.type.js";
 
 export const create = async ({ data }: { data: CreatePropertyDto }) => {
-  const createPayload = {
-    tenant_id: data.tenantId,
-    category_id: data.categoryId,
-    title: data.title,
-    description: data.description,
-    country: data.country,
-    city: data.city,
-    full_address: data.fullAddress,
-    latitude: data.latitude,
-    longitude: data.longitude,
-    number_of_bathrooms: data.numberOfBathrooms,
-  };
-
   const category = await prisma.propertyCategory.findUnique({
-    where: { id: createPayload.category_id },
+    where: { id: data.categoryId },
   });
 
   if (!category) throw new AppError(404, "Category not found");
 
   const property = await prisma.property.create({
-    data: createPayload,
+    data: { ...data },
   });
 
   return property;
